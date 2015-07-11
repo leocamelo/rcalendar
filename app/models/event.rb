0ff417e96ed06584313e0276afb821768948305d
Leonardo Camelo <leonardocamelo.nave@gmail.com>
2015-07-11 19:04:39 -0300

    order events by start_date

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,8 @@
 class Event < ActiveRecord::Base
 
+  # order the events by start_date
+  default_scope order :start_date
+
   # requires title, start_date and end_date
   validates :title, presence: true
   validates :start_date, presence: true
