6e2a917d34208e6a38283011ced91b454c732e23
Leonardo Camelo <leonardocamelo.nave@gmail.com>
2015-07-13 09:25:35 -0300

    specs examples#index

--- spec/controllers/examples_controller_spec.rb
+++ spec/controllers/examples_controller_spec.rb
@@ -3,6 +3,15 @@ require 'rails_helper'
 RSpec.describe ExamplesController, type: :controller do
 
   describe 'GET #index' do
+    before { get :index }
+
+    it 'responds with HTTP 200 status code' do
+      expect(response).to have_http_status(200)
+    end
+
+    it 'renders the index template' do
+      expect(response).to render_template('index')
+    end
   end
 
 end
